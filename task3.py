0051da635c8bab59673b0f85eea999688eadb1b0
sfincks <sfincks666@gmail.com>
2023-04-15 19:33:26 +0500

    task2

--- task3.py
+++ task3.py
@@ -1,1 +1,16 @@
+
 # Задача 14: Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
+# 10 -> 1 2 4 8
+
+n = int(input("Введите число n: "))
+sum = []
+temp = 0
+i = 0
+while temp <= n:
+    temp = 2**i
+    if temp > n:
+        break
+    else:
+        i += 1
+        sum.append(temp)
+print(*sum)
